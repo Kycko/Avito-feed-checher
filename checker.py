1b6f6783b25de76b4b0206099bd9af1a2613f847
Anthony Samartsev <ant.samarcev@gmail.com>
2021-11-28 19:08:19 +0300

    Reading files + making ID list

--- checker.py
+++ checker.py
@@ -9,7 +9,7 @@ def FUNC_exit_with_error(error_code):
     print("Exit...")
     sys.exit()
 def FUNC_read_file(filename):
-    file = open(filename, 'r', encoding='utf-8')
+    file = open(filename, 'r', encoding='utf-8-sig')
     data = file.readlines()
     file.close()
     return data
@@ -45,67 +45,90 @@ print("Prepare...")
 
 if len(sys.argv) == 1:
     FUNC_exit_with_error("PLEASE ADD A FILENAME")
-if not OSpath.isfile(sys.argv[1]):
-    FUNC_exit_with_error("NOT FOUND: "+sys.argv[1])
-DATA_original = FUNC_read_file(sys.argv[1])
-temp = FUNC_find_substring_return_before(".txt", sys.argv[1])
-NEW_filename = temp+" formatted.csv"
+for i in [1, 2]:
+    if not OSpath.isfile(sys.argv[i]):
+        FUNC_exit_with_error("NOT FOUND: "+sys.argv[i])
 
-# making the first few mandatory lines
-print("Making the first few mandatory lines...")
+print("Reading files...")
+FEED_original = FUNC_read_file(sys.argv[1])
+TEMP = FUNC_read_file(sys.argv[2])
 
-for string in DATA_original:
-    tempTEXT = FUNC_find_substring_return_after("string m_Name = ", string, 1)
-    if tempTEXT:
-        DATA_new = ['Key,' + tempTEXT[1:-1] + ',NOTES,' + NEW_filename + ',\n',
-                    ',,,,,,,,,,,,,,,,,,,\n',
-                    'UseCyrillicFont,No,"Пометка для переводчиков: поставьте на позиции (между разделителями) вашего языка «Yes», если используете кириллицу",Yes,\n',
-                    ',,,,,,,,,,,,,,,,,,,\n']
-        break
+print("Making ID list...")
+ID_list = {}
+for string in TEMP:
+    index = string.rfind(",")
 
-# check total amount of KEYS
-temp_counter = False
-for string in DATA_original:
-    tempTEXT = FUNC_find_substring_return_after("int size = ", string, 1)
-    if tempTEXT:
-        if temp_counter:
-            print("KEYS total....................."+tempTEXT)
-            break
-        else:
-            temp_counter = True
+    temp_key = string[:index].replace('""','"')
+    if temp_key[0] == '"':
+        temp_key = temp_key[1:]
+    if temp_key[len(temp_key)-1] == '"':
+        temp_key = temp_key[:len(temp_key)-1]
 
-# making KEYS + ingame strings
-print("Making KEYS + ingame strings...", end="")
+    temp_value = string[index+1:len(string)-1]
+    ID_list[temp_key] = temp_value
 
-string_counter = 0
-key_is_opened = False
 
-for string in DATA_original:
-    if key_is_opened:
-        if string[0] == '\t':
-            DATA_new[-1] += ',,,\n'
-            key_is_opened = False
-        else:
-            DATA_new[-1] += '\n'
-            DATA_new.append(string[:-1])
-    else:
-        tempTEXT = FUNC_find_substring_return_after("string m_Key = ", string, 1)
-        if tempTEXT:
-            string_counter += 1
-            print("\rMaking KEYS + ingame strings..."+str(string_counter), end="")
-            DATA_new.append(tempTEXT[1:-1]+',')
-        elif string_counter:
-            tempTEXT = FUNC_find_substring_return_after("string data = ", string, 1)
-            if tempTEXT:
-                DATA_new[-1] += tempTEXT
-                key_is_opened = True
+for key in ID_list.keys():
+    print(ID_list[key] + " : " + key)
 
-# finalizing
-print()
-print("Writing to the file...")
-FUNC_write_to_the_file(DATA_new, NEW_filename)
-print()
-print("------------------------------")
-print("DONE! Check this file:")
-print(" "+NEW_filename)
-print("------------------------------")
+# temp = FUNC_find_substring_return_before(".txt", sys.argv[1])
+# NEW_filename = temp+" formatted.csv"
+#
+# # making the first few mandatory lines
+# print("Making the first few mandatory lines...")
+#
+# for string in DATA_original:
+#     tempTEXT = FUNC_find_substring_return_after("string m_Name = ", string, 1)
+#     if tempTEXT:
+#         DATA_new = ['Key,' + tempTEXT[1:-1] + ',NOTES,' + NEW_filename + ',\n',
+#                     ',,,,,,,,,,,,,,,,,,,\n',
+#                     'UseCyrillicFont,No,"Пометка для переводчиков: поставьте на позиции (между разделителями) вашего языка «Yes», если используете кириллицу",Yes,\n',
+#                     ',,,,,,,,,,,,,,,,,,,\n']
+#         break
+#
+# # check total amount of KEYS
+# temp_counter = False
+# for string in DATA_original:
+#     tempTEXT = FUNC_find_substring_return_after("int size = ", string, 1)
+#     if tempTEXT:
+#         if temp_counter:
+#             print("KEYS total....................."+tempTEXT)
+#             break
+#         else:
+#             temp_counter = True
+#
+# # making KEYS + ingame strings
+# print("Making KEYS + ingame strings...", end="")
+#
+# string_counter = 0
+# key_is_opened = False
+#
+# for string in DATA_original:
+#     if key_is_opened:
+#         if string[0] == '\t':
+#             DATA_new[-1] += ',,,\n'
+#             key_is_opened = False
+#         else:
+#             DATA_new[-1] += '\n'
+#             DATA_new.append(string[:-1])
+#     else:
+#         tempTEXT = FUNC_find_substring_return_after("string m_Key = ", string, 1)
+#         if tempTEXT:
+#             string_counter += 1
+#             print("\rMaking KEYS + ingame strings..."+str(string_counter), end="")
+#             DATA_new.append(tempTEXT[1:-1]+',')
+#         elif string_counter:
+#             tempTEXT = FUNC_find_substring_return_after("string data = ", string, 1)
+#             if tempTEXT:
+#                 DATA_new[-1] += tempTEXT
+#                 key_is_opened = True
+#
+# # finalizing
+# print()
+# print("Writing to the file...")
+# FUNC_write_to_the_file(DATA_new, NEW_filename)
+# print()
+# print("------------------------------")
+# print("DONE! Check this file:")
+# print(" "+NEW_filename)
+# print("------------------------------")
