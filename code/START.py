baff942b589ef554015af13704abae14349ccee4
Anthony Samartsev <ant.samarcev@gmail.com>
2021-12-02 23:40:13 +0300

    First GUI implementation.

---
+++ code/START.py
@@ -0,0 +1,4 @@
+import main_lib as LIB
+
+window = LIB.FUNC_GUI_init()
+window.mainloop()
--- code/main_lib.py
+++ code/main_lib.py
@@ -1,4 +1,8 @@
+from os import path as OSpath
 from sys import exit as SYSexit
+from tkinter import *
+from tkinter import filedialog
+from tkinter import scrolledtext
 
 # Basic functions
 def FUNC_exit_with_error(error_code):
@@ -16,3 +20,52 @@ def FUNC_write_to_the_file(data, filename):
     file = open(filename, 'w', encoding='utf-8')
     file.writelines(data)
     file.close()
+
+# Button actions
+def button_1_clicked():
+    file_1 = filedialog.askopenfilename(initialdir= OSpath.dirname(__file__))
+    file_choose_clicked_set_label(lbl_1, file_1)
+def button_2_clicked():
+    file_2 = filedialog.askopenfilename(initialdir= OSpath.dirname(__file__))
+    file_choose_clicked_set_label(lbl_2, file_2)
+def file_choose_clicked_set_label(lbl_num, file_num):
+    lbl_num.config(text=OSpath.basename(file_num))
+    if file_num[-4:] in (".csv", ".txt"):
+        lbl_num.config(foreground="#14963A")
+
+# Making GUI
+def FUNC_GUI_init():
+    window = Tk()
+    window.geometry(str(app_width) + "x" + str(app_height))
+    window.title(window_title)
+    window.resizable(0,0)                                       # makes the app window unresizable
+
+    # First button group
+    global lbl_1
+    button_1 = Button(window, text="Выбрать список названий с Type ID", command=button_1_clicked, padx=10, pady=3, height = 1, width = 33)
+    button_1.grid(column=0, row=0, pady=15, sticky=E)
+    lbl_1 = Label(window, text=lbl_1_2_text, foreground=lbl_1_2_colour, padx=22, pady=10, width=50, anchor='w')
+    lbl_1.grid(column=1, row=0, sticky=W)
+
+    # Second button group
+    global lbl_2
+    button_2 = Button(window, text="Выбрать список OEM-номеров с Type ID", command=button_2_clicked, padx=10, pady=3, height = 1, width = 33)
+    button_2.grid(column=0, row=1, sticky=E)
+    lbl_2 = Label(window, text=lbl_1_2_text, foreground=lbl_1_2_colour, padx=22, pady=10, width=50, anchor='w')
+    lbl_2.grid(column=1, row=1, sticky=W)
+
+    # Text field for client's feed
+    clients_feed_list = scrolledtext.ScrolledText(window, width=80, height=30)
+    clients_feed_list.grid(column=0, row=2, columnspan=2, padx=5, pady=20)
+    return window
+
+# Global vars
+app_width = 700
+app_height = 700
+window_title = "Avito feed checker (запчасти)"
+file_1 = ""
+file_2 = ""
+
+# Label vars
+lbl_1_2_text = "Не выбрано"
+lbl_1_2_colour = "#D73B41"
