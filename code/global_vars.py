02b1ddae3dd2ae1316d41364e5844978e42f0a12
Anthony Samartsev <ant.samarcev@gmail.com>
2021-12-07 21:16:59 +0300

    Actions for file choosing + separate global_vars file.

---
+++ code/global_vars.py
@@ -0,0 +1,9 @@
+# App version and main window properties
+VERSION = 'v.026'
+app_width  = 670
+app_height = 685
+app_title  = 'Avito feed checker (запчасти) ' + VERSION
+
+# Colors
+COLORS = {'green'   : '#14963A',
+          'red'     : '#D73B41'}
--- code/main_lib.py
+++ code/main_lib.py
@@ -1,19 +1,19 @@
+import global_vars as Globals
 import tkinter as tk
+from os import path as OSpath
+from tkinter import filedialog
 
 class APP(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.win_width  = 670                               # main window size
-        self.win_height = 685                               # main window size
-        self.win_title  = 'Avito feed checker (запчасти)'   # main window title
+        self.geometry(str(Globals.app_width) + "x" + str(Globals.app_height))
+        self.title(Globals.app_title)
+        self.resizable(0,0)                                     # makes the app window unresizable
 
-        self.files = []
+        self.files   = ['', '']                                 # only the path to each file
+        self.IDlists = ['', '']                                 # ID lists as a reference to check
 
-        self.geometry(str(self.win_width) + "x" + str(self.win_height))
-        self.title(self.win_title)
-        self.resizable(0,0)                                 # makes the app window unresizable
-
-        self.buttons = {}                                   # all the buttons of the app
+        self.buttons = {}                                       # all the buttons of the app
         for i in range(2):
             self.buttons['btn'+str(i)] = tk.Button(self, padx=10, pady=3, height=1, width=33)
             self.buttons['btn'+str(i)].grid(column=0, row=i, columnspan=2, padx=7, pady=15-(i*15), sticky=tk.E)
@@ -22,12 +22,27 @@ class APP(tk.Tk):
         self.buttons['btn0']['command'] = self.btn0_clicked
         self.buttons['btn1']['command'] = self.btn1_clicked
 
-        self.labels = {}                                    # all the labels of the app
+        self.labels = {}                                        # all the labels of the app
         for i in range(2):
-            self.labels['lbl'+str(i)] = {}
+            self.labels['lbl'+str(i)] = {'num'      : i,
+                                         'ready'    : False}    # it means "ready to start main processing"
             self.labels['lbl'+str(i)]['obj'] = tk.Label(self, padx=12, pady=5, width=47, anchor='w', relief=tk.GROOVE)
             self.labels['lbl'+str(i)]['obj'].grid(column=2, row=i, columnspan=2, padx=15, sticky=tk.W)
     def btn0_clicked(self):
-        print('btn0_clicked')
+        self.files[0] = filedialog.askopenfilename(initialdir= OSpath.dirname(__file__))
+        self.file_choose_clicked(0)
     def btn1_clicked(self):
-        print('btn1_clicked')
+        self.files[1] = filedialog.askopenfilename(initialdir= OSpath.dirname(__file__))
+        self.file_choose_clicked(1)
+    def file_choose_clicked(self, num):
+        if self.files[num]:
+            text = OSpath.basename(self.files[num])[:40]
+            if self.files[num][-4:] in (".csv", ".txt"):
+                color = Globals.COLORS['green']
+                # final_list_var = FUNC_prepare_ID_list(file)   # to be done later
+                # items_count = len(final_list_var)             # to be done later
+                # text = "[" + str(items_count) + " " + FUNC_plural_word_endings(items_count) + "] " + text
+            else:
+                color = Globals.COLORS['red']
+            self.labels['lbl'+str(num)]['obj'].config(foreground=color, text=text)
+        # FUNC_check_main_start_state()                         # to be done later
