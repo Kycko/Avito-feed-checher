7d35232a518c6a828640af1475fc5de037a842d3
Anthony Samartsev <ant.samarcev@gmail.com>
2021-12-20 18:36:15 +0300

    Join multiple lines BEFORE processing the strings.

--- code/main_functions_lib.py
+++ code/main_functions_lib.py
@@ -11,6 +11,27 @@ def write_to_the_file(data, filename):
     file = open(filename, 'w', encoding='utf-8')
     file.writelines(data)
     file.close()
+def join_multiple_lines(list):
+    result = []
+    NEXTLINE = False
+
+    for line in list:
+        if NEXTLINE:
+            if line.count('"') % 2:
+                NEXTLINE = False
+                if line[:-1] == '"' or line == '"':
+                    line = line[:-1]
+            result[-1] += line
+        elif line.count('"') % 2:
+            NEXTLINE = True
+            result.append(line[1:])
+        else:
+            result.append(line)
+
+    for i in range(len(result)):
+        result[i] = result[i].replace('""','"')                  # кавычки удваиваются при переносе текста
+
+    return result
 def copy_to_clipboard(data):
     result = ''
     for line in data:
@@ -32,10 +53,23 @@ def plural_word_endings(num, dict):             # num: real number, dict: what d
               any((num % 100 < 10, num % 100 >= 20)))):
         return words[dict][1]
     return words[dict][2]
+def del_start_end_quotes(string):
+    if string[0] == '"':
+        string = string[1:]
+    if string[-1] == '"':
+        string = string[:-1]
+    return string
+def del_enters_in_list(list):
+    for i in range(len(list)):
+        if list[i][-1] == '\n':
+            list[i] = list[i][:-1]
+    return list
 
 # File checking
 def prepare_ID_list(file):
     TEMP = read_file(file)
+    TEMP = del_enters_in_list(TEMP)
+    TEMP = join_multiple_lines(TEMP)
     ID_list = {}
 
     for line in TEMP:
@@ -44,23 +78,24 @@ def prepare_ID_list(file):
             line = line.lower()                                 # уменьшаем все буквы
             index = line.rfind(",")
             temp_key = line[:index].replace('""','"')           # кавычки удваиваются при экспорте в .csv
-            if temp_key[0] == '"':
-                temp_key = temp_key[1:]
-            if temp_key[-1] == '"':
-                temp_key = temp_key[:-1]
+            temp_key = del_start_end_quotes(temp_key)
             if line[-1] == "\n":
                 line = line[:-1]
 
             temp_value = line[index+1:]
+            temp_value = del_start_end_quotes(temp_value)
             ID_list[temp_key] = temp_value
+    print(ID_list)                              # for DEBUG
     return ID_list
 def prepare_FEED_original(list):
+    list = join_multiple_lines(list)
     final_list = []
     NEXTLINE = False
 
     for line in list:
         # print(line)                           # for DEBUG
         line = line.lower()                     # уменьшаем все буквы
+        line = line.replace('""','"')   # кавычки удваиваются при копировании
         # print('"' + line + '"')               # for DEBUG
         if line:
             if NEXTLINE:
@@ -81,6 +116,7 @@ def prepare_FEED_original(list):
                 final_list.append(line)
         elif not NEXTLINE:
             final_list.append(line)
+    print(final_list)                           # for DEBUG
     return final_list
 def MAIN_CYCLE(FEED_original, ID_list):
     FINAL_ID_list = []
